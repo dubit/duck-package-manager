a91d6c9e4126f8172abaaa6eb81ef09964b121fb
Jamie Healey <jamie.healey@dubitlimited.com>
2018-06-12 10:49:01 +0100

    added error message + validation

--- Editor/UI/Components/AddCustomPackage/AddCustomPackagePage.cs
+++ Editor/UI/Components/AddCustomPackage/AddCustomPackagePage.cs
@@ -1,5 +1,9 @@
-﻿using DUCK.PackageManager.Editor.UI.Styles;
+﻿using System.Linq;
+using DUCK.PackageManager.Editor.UI.Stores;
+using DUCK.PackageManager.Editor.UI.Styles;
 using DUCK.PackageManager.Editor.UI.Utils;
+using UnityEditor.Timeline;
+using UnityEngine;
 using UnityEngine.Experimental.UIElements;
 using UnityEngine.Experimental.UIElements.StyleEnums;
 
@@ -7,23 +11,31 @@ namespace DUCK.PackageManager.Editor.UI.Components.AddCustomPackage
 {
 	public class AddCustomPackagePage : VisualElement
 	{
+		private TextField nameInput;
+		private TextField urlInput;
+		private Label errorLabel;
+
 		public AddCustomPackagePage()
 		{
 			style.flex = 1;
 			style.paddingTop = 16;
 
 			var nameField = new Label("Package Name (Can be anything as long as it's unique)");
-			var nameInput = new TextField();
+			nameInput = new TextField();
 			nameInput.style.Margin(4);
 			nameInput.style.Padding(4);
 			nameInput.style.fontSize = FontSizes.textInput;
 
-			var urlLabel = new Label("Git URL");
+			var urlLabel = new Label("Git URL (Must be https)");
 			urlLabel.style.marginTop = 4;
-			var urlInput = new TextField();
+			urlInput = new TextField();
 			urlInput.style.Margin(4);
 			urlInput.style.Padding(4);
 			urlInput.style.fontSize = FontSizes.textInput;
+
+			errorLabel = new Label();
+			errorLabel.style.marginTop = 4;
+			errorLabel.style.marginBottom = 4;
 
 			var addButton = new Button(HandleAddButtonClicked);
 			addButton.text = "Add";
@@ -35,13 +47,46 @@ namespace DUCK.PackageManager.Editor.UI.Components.AddCustomPackage
 			Add(nameInput);
 			Add(urlLabel);
 			Add(urlInput);
+			Add(errorLabel);
 			Add(addButton);
-
-			Add(new Overlay(new LoadingIndicator("Coming Soon")));
 		}
 
 		private void HandleAddButtonClicked()
 		{
+			// clear error
+			errorLabel.text = "";
+
+			var name = nameInput.value;
+			var url = urlInput.value;
+
+			if (string.IsNullOrEmpty(name))
+			{
+				errorLabel.text = "Error! Package name cannot be empty";
+				return;
+			}
+
+			var installedPackages = RootStore.Instance.Packages.InstalledPackages;
+			// name must not be already used by another installed package
+			if (installedPackages.Value.Packages.Any(p => p.Name == name))
+			{
+				errorLabel.text = "Error! A package with this name already exists";
+				return;
+			}
+
+			if (string.IsNullOrEmpty(url))
+			{
+				errorLabel.text = "Error! Git url cannot be empty";
+				return;
+			}
+
+			// url must start with https and end
+			if (!url.StartsWith("https://") || !url.EndsWith(".git"))
+			{
+				errorLabel.text = "Error! git url must be a https git url";
+				return;
+			}
+
+			// If we get to here we can install
 		}
 	}
 }
