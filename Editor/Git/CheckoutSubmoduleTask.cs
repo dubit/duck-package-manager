a2483a33efb434b63344b6d85fcb0a70321c25c3
Jamie Healey <jamie.healey@dubitlimited.com>
2018-07-25 16:54:39 +0100

    Allow AbstractProcessTask subclass to handle results themselves

--- Editor/Git/CheckoutSubmoduleTask.cs
+++ Editor/Git/CheckoutSubmoduleTask.cs
@@ -1,13 +1,43 @@
-﻿namespace DUCK.PackageManager.Editor.Git
+﻿using System.IO;
+using System.Linq;
+using DUCK.PackageManager.Editor.Tasks;
+
+namespace DUCK.PackageManager.Editor.Git
 {
 	internal class CheckoutSubmoduleTask : GitTask
 	{
+		private readonly string version;
 		private const string COMMAND = "checkout {0}";
 
 		public CheckoutSubmoduleTask(string installDirectory, string version) :
 			base(string.Format(COMMAND, version))
 		{
+			this.version = version;
 			WorkingDirectory = installDirectory;
+		}
+
+		protected override ProcessTaskResult HandleResult(ProcessTaskResult result)
+		{
+			if (result.ExitCode != 0)
+			{
+				if (result.StdErr.Count > 0)
+				{
+					var folderName = WorkingDirectory.Split(Path.AltDirectorySeparatorChar).Last();
+					var errMessage = result.StdErr[0];
+					var message = string.Format(
+						"Failed to checkout submodule: {0}:{1}\r\n{2}",
+						folderName,
+						version,
+						errMessage);
+					result.Err(message);
+				}
+				else
+				{
+					result.Err();
+				}
+			}
+
+			return result;
 		}
 	}
 }
